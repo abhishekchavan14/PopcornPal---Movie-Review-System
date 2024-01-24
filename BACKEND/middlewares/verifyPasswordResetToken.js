bfd462ebc4180311a441cfaa19d8824952848bf2
Abhishek Chavan <abhishekchavan940@gmail.com>
2024-01-24 16:52:56 +0530

    Done: Password reset verification. To be done:  To change the password in DB

---
+++ BACKEND/middlewares/verifyPasswordResetToken.js
@@ -0,0 +1,21 @@
+const PasswordResetToken = require('../models/passwordResetToken')
+
+//creating our middleware function
+exports.isValidPassResetToken = async (req,res,next) => {
+    const {token, userID} = req.body
+
+    //checking if there is any token or not for that user
+    const resetToken = await PasswordResetToken.findOne({ownerID: userID})
+    //if there is not
+    if(!resetToken) {return res.status(404).json({error: 'Generate new token.'})}
+
+    //if there is
+    const matched = await resetToken.compareToken(token) //we defined compareToken function in models/passwordResetToken.js
+
+    //if the token doesnt match
+    if(!matched) {return res.status(401).json({error: 'Invalid access!'})}
+
+    //if token is valid and matched
+    // req.resetToken = resetToken --> idk why to do this?
+    next()
+}
--- BACKEND/routes/user.js
+++ BACKEND/routes/user.js
@@ -3,6 +3,8 @@ const {validate, userValidator} = require("../middlewares/validator")
 const {} = require("../middlewares/validator")
 
 const { create, verifyEmail, forgetPassword } = require("../controllers/user")
+
+const{isValidPassResetToken} = require("../middlewares/verifyPasswordResetToken")
 const router = express.Router()
 
 router.post('/verify-email', verifyEmail) //route to email verification (token comparison actually)
@@ -10,5 +12,9 @@ router.post('/verify-email', verifyEmail) //route to email verification (token c
 router.post('/create', userValidator, validate, create) //create is the actual route handler/controller that will be executed if the request passes through the previous middlewares without errors.
 
 router.post('/forget-password', forgetPassword)
+
+router.post('/verify-pass-reset-token', isValidPassResetToken, (req, res) =>{
+    res.json({valid:true})
+}) //isValidPassResetToken is a middleware 
 
 module.exports = router
